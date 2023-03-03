362e854dcc22ada34c64eb7eb3d11b6c7f49afc2
sitecore-jbe <jbe@sitecore.net>
2023-03-04 00:41:28 +0100

    Update DemoToolInfoSlider.js

--- Production/DemoToolInfoSlider.js
+++ Production/DemoToolInfoSlider.js
@@ -856,7 +856,7 @@ function InitReplacers(data) {
 
             demoToolData.Replacers.location.timezone = response.time_zone;
             let timezoneName = demoToolData.Replacers.location.timezone.name;
-            demoToolData.Replacers.location.timezone.current_time = CurrentDateTimeInTimezoneLocaleString(timezoneName);
+            demoToolData.Replacers.location.timezone.current_time = new Date(CurrentDateTimeInTimezoneLocaleString(timezoneName));
             demoToolData.Replacers.location.timezone.timeOfTheDay = TimeOfTheDay(CurrentTimeInTimezone(timezoneName));
 
             resolve(demoToolData);
@@ -1183,9 +1183,9 @@ function ComposeDemoToolProperty(propertyDefinitionPath, parentHtmlElement) {
             // Initiate Timezone Time Interval
             setInterval(() => {
                 let timezoneName = demoToolData.Replacers.location.timezone.name;
-                demoToolData.Replacers.location.timezone.current_time = CurrentDateTimeInTimezoneLocaleString(timezoneName);
+                demoToolData.Replacers.location.timezone.current_time = new Date(CurrentDateTimeInTimezoneLocaleString(timezoneName));
                 demoToolData.Replacers.location.timezone.timeOfTheDay = TimeOfTheDay(CurrentTimeInTimezone(timezoneName));
-                propertyValue.text = FormatDataByDataType(displayValue, dataType);
+                propertyValue.text = FormatDataByDataType(demoToolData.Replacers.location.timezone.current_time, dataType);
             }, 1000);
         }
 
